34e05bf96f6d9b291fc55baa7fdef53d66fc7f86
virusold <virusold0607@gmail.com>
2022-11-26 23:48:04 +0000

    Update detail.blade.php

--- resources/views/service/detail.blade.php
+++ resources/views/service/detail.blade.php
@@ -39,46 +39,48 @@
   <div class="py-9">
       <div class="container">
           <div class="row">
-            <div class="col-md-8">
-                <h3>Service Detail</h3>
-            </div>
-            <div class="col-md-8 carousel mx-auto">
-              <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner">
-                  @for ($i = 0; $i < count($service->galleries); ++$i)
-                  <div class="carousel-item {{ $i == 0 ? "active" : "" }}">
-                    <img src="/uploads/all/{{$service->galleries[$i]->file_name}}" class="d-block w-100" alt="..." />
-                  </div>
-                  @endfor
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                </a>
-                <ol class="carousel-indicators">
+            <div class="col-lg-9">
+              <div class="carousel">
+                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                  <div class="carousel-inner">
                     @for ($i = 0; $i < count($service->galleries); ++$i)
-                    <li data-target="#carouselExampleIndicators" data-slide-to="{{$i}}" class="{{$i == 0 ? "active": "" }}">
-                        <img src="/uploads/all/{{$service->galleries[$i]->file_name}}" class="d-block w-100">
-                    </li>
+                    <div class="carousel-item {{ $i == 0 ? "active" : "" }}">
+                      <img src="/uploads/all/{{$service->galleries[$i]->file_name}}" class="d-block w-100" alt="..." />
+                    </div>
                     @endfor
-                </ol>
+                  </div>
+                  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  </a>
+                  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  </a>
+                  <ol class="carousel-indicators">
+                      @for ($i = 0; $i < count($service->galleries); ++$i)
+                      <li data-target="#carouselExampleIndicators" data-slide-to="{{$i}}" class="{{$i == 0 ? "active": "" }}">
+                          <img src="/uploads/all/{{$service->galleries[$i]->file_name}}" class="d-block w-100">
+                      </li>
+                      @endfor
+                  </ol>
+                </div>
               </div>
-            </div>
-            <div class="col-md-8 mb-3">
-              <h4>{{$service->name}}</h4>
-              @if ($service->count > 0)
-              <div>
-                <span><i class="bi bi-star-fill fs-20 text-warning"></i> {{ $service->rating ?: "0.0" }}</span>
-                <span class="text-secondary">({{$service->count}})</span>
+
+              <div class="mb-2">
+                <h4>{{$service->name}}</h4>
+                @if ($service->count > 0)
+                <div>
+                  <span><i class="bi bi-star-fill fs-20 text-warning"></i> {{ $service->rating ?: "0.0" }}</span>
+                  <span class="text-secondary">({{$service->count}})</span>
+                </div>
+                @endif
               </div>
-              @endif
+              <div class="mb-2">
+                <h4>Content</h4>
+                <div>{!! $service->content !!}</div>
+              </div>
+
             </div>
-            <div class="col-md-8">
-              <h4>Content</h4>
-              <div>{!! $service->content !!}</div>
-            </div>
+            
             <div class="col-md-12 row">
               <h4>Packages</h4>
               <div class="col-span-5 col-md-3">
