89225dd14f336a43b38a0912b5f26f36d9731ed3
virusold <virusold0607@gmail.com>
2022-12-10 17:31:37 +0000

    Update header.blade.php

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -199,8 +199,8 @@
                         @foreach ($message_notifications as $message_notification)
                         <a href="{{ env('APP_URL')}}/chat/{{$message_notification->user_id }}" class="filterDiscussions all unread single active d-block py-2 border-bottom text-black"  data-toggle="list" role="tab">
                             <div class="row">
-                                <div class="d-flex align-items-center">
-                                    <div class="mr-5px w-50px">
+                                <div class="d-flex">
+                                    <div class="mr-10px w-50px">
                                         <img class="w-100 rounded-circle" src="{{user_name($message_notification->user_id)[0]->uploads->getImageOptimizedFullName(100,100)}}" data-toggle="tooltip" data-placement="top" title="Janette" alt="{{user_name($message_notification->user_id)[0]->first_name}} avatar">
                                     </div>
                                     <div class="col- fs-14 fw-700">
