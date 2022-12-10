72b4c77f1141e5a1e0890d6345be5496915fd3c9
virusold <virusold0607@gmail.com>
2022-12-10 14:20:31 +0000

    Update detail.blade.php

--- resources/views/service/detail.blade.php
+++ resources/views/service/detail.blade.php
@@ -37,66 +37,67 @@
   <div class="py-9">
       <div class="container">
           <div class="row">
-            <div class="col-lg-6">
-              <div class="mb-2">
-                <h1 class="fs-24">{{$service->name}}</h4>
-                @if ($service->count > 0)
-                <div class="">
-                  <span><i class="bi bi-star-fill fs-20 text-warning"></i> {{ $service->rating ?: "0.0" }}</span>
-                  <span class="text-secondary">({{$service->count}})</span>
-                </div>
-                @endif
-              </div>
-
-              <div class="carousel">
-                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                  <div class="carousel-inner">
-                    @for ($i = 0; $i < count($service->galleries); ++$i)
-                    <div class="carousel-item {{ $i == 0 ? "active" : "" }}">
-                      <img src="/uploads/all/{{$service->galleries[$i]->file_name}}" class="d-block w-100 border" alt="..." />
-                    </div>
-                    @endfor
+            <div class="col-xl-11 mx-auto">
+              <div class="col-lg-6">
+                <div class="mb-2">
+                  <h1 class="fs-24 mb-4">{{$service->name}}</h4>
+                  @if ($service->count > 0)
+                  <div class="">
+                    <span><i class="bi bi-star-fill fs-20 text-warning"></i> {{ $service->rating ?: "0.0" }}</span>
+                    <span class="text-secondary">({{$service->count}})</span>
                   </div>
-                  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  </a>
-                  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  </a>
-                  <ol class="carousel-indicators">
+                  @endif
+                </div>
+
+                <div class="carousel">
+                  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
                       @for ($i = 0; $i < count($service->galleries); ++$i)
-                      <li data-target="#carouselExampleIndicators" data-slide-to="{{$i}}" class="{{$i == 0 ? "active": "" }}">
-                          <img src="/uploads/all/{{$service->galleries[$i]->file_name}}" class="d-block w-100 border">
-                      </li>
+                      <div class="carousel-item {{ $i == 0 ? "active" : "" }}">
+                        <img src="/uploads/all/{{$service->galleries[$i]->file_name}}" class="d-block w-100 border" alt="..." />
+                      </div>
                       @endfor
-                  </ol>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    </a>
+                    <ol class="carousel-indicators">
+                        @for ($i = 0; $i < count($service->galleries); ++$i)
+                        <li data-target="#carouselExampleIndicators" data-slide-to="{{$i}}" class="{{$i == 0 ? "active": "" }}">
+                            <img src="/uploads/all/{{$service->galleries[$i]->file_name}}" class="d-block w-100 border">
+                        </li>
+                        @endfor
+                    </ol>
+                  </div>
                 </div>
-              </div>
 
-              <div class="mb-2">
-                <h4>Content</h4>
-                <div>{!! $service->content !!}</div>
-              </div>
-
-            </div>
-            
-            <div class="col-lg-6">
-              <div class="row">
-                <h4>Packages</h4>
-                @foreach ($service->packages as $package)
-                <div class="col-span-5 col-lg-4">
-                  <h3>${{number_format($package->price / 100, 2)}}</h3>
-                  <h4>{{$package->name}}</h4>
-                  <p>{{$package->description}}</p>
-                  <p>{{$package->delivery_time}} Day Delivery</p>
-                  <p>{{$package->revisions}} Revisions</p>
-                  <a href="/services/checkout/{{$package->id}}" type="button" class="btn btn-info">Continue(${{number_format($package->price / 100, 2)}})</a>
+                <div class="mb-2">
+                  <h4>Content</h4>
+                  <div>{!! $service->content !!}</div>
                 </div>
-                @endforeach
-              </div>
 
-            </div>
-            
+              </div>
+              
+              <div class="col-lg-6">
+                <div class="row">
+                  <h4>Packages</h4>
+                  @foreach ($service->packages as $package)
+                  <div class="col-span-5 col-lg-4">
+                    <h3>${{number_format($package->price / 100, 2)}}</h3>
+                    <h4>{{$package->name}}</h4>
+                    <p>{{$package->description}}</p>
+                    <p>{{$package->delivery_time}} Day Delivery</p>
+                    <p>{{$package->revisions}} Revisions</p>
+                    <a href="/services/checkout/{{$package->id}}" type="button" class="btn btn-info">Continue(${{number_format($package->price / 100, 2)}})</a>
+                  </div>
+                  @endforeach
+                </div>
+
+              </div>
+            </div>  
           </div>
       </div>
   </div>
