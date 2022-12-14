097a0e4f5f3f24eaaab4ecd39fd1657472d81d38
virusold <virusold0607@gmail.com>
2022-12-14 20:24:38 +0000

    Update detail.blade.php

--- resources/views/service/detail.blade.php
+++ resources/views/service/detail.blade.php
@@ -152,8 +152,7 @@
                                         <p>{{$package->delivery_time}} Day Delivery</p>
                                         <p>{{$package->revisions}} Revisions</p>
                                         <a href="/services/checkout/{{$package->id}}" type="button"
-                                        class="btn btn-info">Continue(${{number_format($package->price / 100, 2)}}
-                                            )</a>
+                                        class="btn btn-primary">Continue</a>
                                     </div>
                                 @endforeach
                             </div>
