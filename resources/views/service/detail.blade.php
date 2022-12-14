d3040b2af0ea773b90eb33f99f73fd9876bbcaf7
virusold <virusold0607@gmail.com>
2022-12-14 20:38:28 +0000

    Update detail.blade.php

--- resources/views/service/detail.blade.php
+++ resources/views/service/detail.blade.php
@@ -92,7 +92,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="card mt-2">
+                            <div class="card mt-3">
                                 <div class="card-body">
                                     <div class="row">
                                         <div class="col mb-3">
