f4e31dfcf9369c0b8cc6b612178c26ebc7f2d14a
virusold <virusold0607@gmail.com>
2022-12-31 18:50:18 +0000

    Update validation-form.blade.php

--- resources/views/includes/validation-form.blade.php
+++ resources/views/includes/validation-form.blade.php
@@ -1,17 +1,13 @@
 @if ($message = Session::get('success'))
 <div class="alert alert-success alert-dismissible fade show" role="alert">
-  <strong>{{ $message }}</strong>
+  {{ $message }}
   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
 @endif
 
 
 @if ($errors->any())
-
-    <ul class="errors text-danger p-0">
-        @foreach ($errors->all() as $error)
-            <li class="error d-flex justify-content-center">{{ $error }}</li>
-        @endforeach
-    </ul>
-
+    @foreach ($errors->all() as $error)
+        <p>{{ $error }}</p>
+    @endforeach
 @endif
