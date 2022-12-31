f11349febf237ba6ccfe02c433a9550681207ff7
virusold <virusold0607@gmail.com>
2022-12-31 21:40:54 +0000

    Update orders.blade.php

--- resources/views/courses/orders.blade.php
+++ resources/views/courses/orders.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout page-title="My Orders">
   <div class="container">
-      <div class="col-lg-8 col-md-10 py-8 mx-auto checkout-wrap">
+      <div class="col-lg-8 col-md-10 pt-9 pb-8 mx-auto checkout-wrap">
           <h1 class="fw-800">Order history</h1>
           <p class="pb-4">Check the status of recent orders, view order details, and fufilment status.</p>
           
