9e24b2eb71a2e06042916d800926d97f6402947d
virusold <virusold0607@gmail.com>
2023-01-06 14:52:56 +0000

    Update show.blade.php

--- resources/views/courses/take/show.blade.php
+++ resources/views/courses/take/show.blade.php
@@ -4,23 +4,27 @@
             <div class="col-xl-10 mx-auto">
                 <div class="row">
                     <div class="col-lg-4">
-                        @foreach ($course->lessons as $i => $lesson)
-                        <div class="nav-item">
-                            <a class="nav-link nav-link-main dropdown-toggle fs-4 pt-2 pb-2" href="#navbar" role="button" data-bs-toggle="collapse" data-bs-target="#navbar-{{$i}}" aria-expanded="true">
-                              <span class="nav-link-title">{{ $lesson->name }}</span>
-                            </a>
-                            <div id="navbar-{{$i}}" class="nav-collapse collapse show">
-                                @foreach ($lesson->contents as $j => $content)
-                                <div class="d-flex align-items-center icon-{{$i}}-{{$j}}">
-                                    <a class="nav-link fs-5 lesson" role="button" href="{{ route('courses.take', ['slug'=>Request::route('slug'), 'content' => $content->id]) }}">{{ $content->name }}</a>
-                                    @if ($content->history)
-                                    <i class="bi bi-check-lg"></i>
-                                    @endif
+                        <div class="card">
+                            <div class="card-body">
+                                @foreach ($course->lessons as $i => $lesson)
+                                <div class="nav-item">
+                                    <div class="fw-700 fs-20 pt-2 pb-2" href="#navbar" role="button" data-bs-toggle="collapse" data-bs-target="#navbar-{{$i}}" aria-expanded="true">
+                                    <span class="nav-link-title">{{ $lesson->name }}</span>
+                                    </div>
+                                    <div id="navbar-{{$i}}" class="nav-collapse collapse show">
+                                        @foreach ($lesson->contents as $j => $content)
+                                        <div class="d-flex align-items-center icon-{{$i}}-{{$j}}">
+                                            <a class="nav-link fs-16 fw-700 lesson" role="button" href="{{ route('courses.take', ['slug'=>Request::route('slug'), 'content' => $content->id]) }}">{{ $content->name }}</a>
+                                            @if ($content->history)
+                                            <i class="bi bi-check-lg text-success"></i>
+                                            @endif
+                                        </div>
+                                        @endforeach
+                                    </div>
                                 </div>
                                 @endforeach
                             </div>
                         </div>
-                        @endforeach
                     </div>
                     <div class="col-lg-8">
                         <h1 class="fw-800 mb-4 title">{!! $course->name !!}</h1>
