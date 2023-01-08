754bc02eab4fdb92e2b5af9db0d2e38e1498cacb
virusold <virusold0607@gmail.com>
2023-01-08 23:54:31 +0000

    Update pricing.blade.php

--- resources/views/seller/services/step/pricing.blade.php
+++ resources/views/seller/services/step/pricing.blade.php
@@ -1,4 +1,3 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" ></script>
 <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
 <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
 
