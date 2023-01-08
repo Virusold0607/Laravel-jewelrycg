e3344b577fbfd17f34e65a7a24034bc57b7c2647
virusold <virusold0607@gmail.com>
2023-01-08 17:25:43 +0000

    Update seller_profile.blade.php

--- resources/views/seller_profile.blade.php
+++ resources/views/seller_profile.blade.php
@@ -24,11 +24,11 @@
         </div>
         <div class="col-lg-9">
           <div class="seller-products-card">
-            <div class="seller-products-card-header fs-20 fw-700 mb-3">Products</div>
+            <div class="seller-products-card-header fs-20 fw-700 mb-3">Our Products</div>
             <div class="seller-products-card-body">
               <div class="row">
                   @foreach ($products as $product)
-                  <div class="mb-4 col-lg-4 col-md-6 col-sm-12">
+                  <div class="mb-4 col-lg-3">
                     <div class="card">
                       <div class="card-body">
                         <img src="{{ $product->uploads->getImageOptimizedFullName(400,400) }}" alt="{{ $product->name }}" class="w-100 img-fluid">
@@ -44,11 +44,11 @@
             </div>
           </div>
           <div class="seller-services-card">
-            <div class="seller-services-card-header fs-20 fw-700 mb-3">Services</div>
+            <div class="seller-services-card-header fs-20 fw-700 mb-3">Our Services</div>
             <div class="seller-services-card-body">
               <div class="row">
                 @foreach ($services as $service)
-                <div class="mb-4 col-lg-4 col-md-6 col-sm-12">
+                <div class="mb-4 col-lg-3">
                   <div class="card">
                     <div class="card-body">
                       <img src="{{ $service->uploads->getImageOptimizedFullName(400,400) }}" alt="{{ $service->name }}" class="w-100 img-fluid">
