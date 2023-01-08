3b90a48f78ede6af94b93c706b9806dabe45874f
virusold <virusold0607@gmail.com>
2023-01-08 17:26:30 +0000

    Update seller_profile.blade.php

--- resources/views/seller_profile.blade.php
+++ resources/views/seller_profile.blade.php
@@ -31,7 +31,7 @@
                   <div class="mb-4 col-lg-3">
                     <div class="card">
                       <div class="card-body">
-                        <img src="{{ $product->uploads->getImageOptimizedFullName(400,400) }}" alt="{{ $product->name }}" class="w-100 img-fluid">
+                        <img src="{{ $product->uploads->getImageOptimizedFullName(400,400) }}" alt="{{ $product->name }}" class="border w-100 img-fluid">
                         <h5 class="my-3">{{ $product->name }}</h5>
                         <p class="text-muted mb-1">{{ $product->description }}</p>
                         <p class="text-muted mb-4">{{ $product->product_category->name }}</p>
@@ -51,7 +51,7 @@
                 <div class="mb-4 col-lg-3">
                   <div class="card">
                     <div class="card-body">
-                      <img src="{{ $service->uploads->getImageOptimizedFullName(400,400) }}" alt="{{ $service->name }}" class="w-100 img-fluid">
+                      <img src="{{ $service->uploads->getImageOptimizedFullName(400,400) }}" alt="{{ $service->name }}" class="border w-100 img-fluid">
                       <h5 class="my-3">{{ $service->name }}</h5>
                       <p class="text-muted mb-1">{{ $service->description }}</p>
                       <p class="text-muted mb-4">{{ join(',' , array_map(function ($item) {return $item['category']['category_name'];}, $service->categories->toArray())) }}</p>
