53c537c2fd38e2b53e8f031e6646d27d7887e30f
virusold <virusold0607@gmail.com>
2023-01-08 22:55:09 +0000

    Update pricing.blade.php

--- resources/views/seller/services/step/pricing.blade.php
+++ resources/views/seller/services/step/pricing.blade.php
@@ -107,8 +107,8 @@
             @csrf
             <div class="card">
                 <div class="card-header">
-                    <h4 class="card-header-title mb-0">Service Package</h4>
-                    <div>
+                    <h4 class="card-header-title mb-0">Packages</h4>
+                    <div class="">
                         Offer Packages: <input data-id="" class="toggle-class" type="checkbox" data-onstyle="success" data-offstyle="danger" data-toggle="toggle" data-on="Active" data-off="InActive" {{count($data->packages) == 3 ? "checked": ""}}>
                     </div>
                 </div>
@@ -121,8 +121,8 @@
                         <div class="w-30">
                             <div class="package-title">BASIC</div>
                             <input type="hidden" name="type[]" value="1"/>
-                            <div class="d-flex sub-content">
-                                <textarea type="text" name="name[]" id="name" rows="4" maxlength="35" class="w-100" placeholder="Name your package">{{isset($packages) ? (count($packages) >= 1 ? $packages[0]->name : "") : "" }}</textarea>
+                            <div class="d-flex sub-content d-none">
+                                <textarea type="text" name="name[]" id="name" rows="4" maxlength="35" class="w-100" value="Basic" placeholder="Name your package">{{isset($packages) ? (count($packages) >= 1 ? $packages[0]->name : "") : "" }}</textarea>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
                                     <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
                                 </svg>
@@ -177,8 +177,8 @@
                         <div class="w-30">
                             <div class="package-title">STANDARD</div>
                             <input type="hidden" name="type[]" value="2"/>
-                            <div class="d-flex sub-content">
-                                <textarea type="text" name="name[]" id="name" rows="4" maxlength="35" class="w-100" placeholder="Name your package">{{isset($packages) ? (count($packages) >= 2 ? $packages[1]->name : "") : "" }}</textarea>
+                            <div class="d-flex sub-content d-none">
+                                <textarea type="text" name="name[]" id="name" rows="4" maxlength="35" class="w-100" value="Standard" placeholder="Name your package">{{isset($packages) ? (count($packages) >= 2 ? $packages[1]->name : "") : "" }}</textarea>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
                                     <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
                                 </svg>
@@ -233,8 +233,8 @@
                         <div class="w-30">
                             <div class="package-title">PREMIUM</div>
                             <input type="hidden" name="type[]" value="3"/>
-                            <div class="d-flex sub-content">
-                                <textarea type="text" name="name[]" id="name" rows="4" maxlength="35" class="w-100" placeholder="Name your package">{{isset($packages) ? (count($packages) >= 3 ? $packages[2]->name : "") : "" }}</textarea>
+                            <div class="d-flex sub-content d-none">
+                                <textarea type="text" name="name[]" id="name" rows="4" maxlength="35" class="w-100" value="Premium" placeholder="Name your package">{{isset($packages) ? (count($packages) >= 3 ? $packages[2]->name : "") : "" }}</textarea>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
                                     <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
                                 </svg>
