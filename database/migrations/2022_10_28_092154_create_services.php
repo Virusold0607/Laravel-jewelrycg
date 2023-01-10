1b36d712449da265ff6364d4b7fc4265d2bfb2e9
virusold <virusold0607@gmail.com>
2023-01-10 00:48:17 +0000

    Update 2022_10_28_092154_create_services.php

--- database/migrations/2022_10_28_092154_create_services.php
+++ database/migrations/2022_10_28_092154_create_services.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('services', function (Blueprint $table) {
             $table->id();
-            $table->integer('status'); // 5 = Deleted/Archived
+            $table->integer('status'); // 1 published, 2 draft, 3 pending review, 4 Deleted/Archived
             $table->integer('user_id');
             $table->string('name');
             $table->string('slug');
