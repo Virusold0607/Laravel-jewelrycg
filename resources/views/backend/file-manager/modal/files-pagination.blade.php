a1a503e467dbc9b211016bf352c5065f557567c8
virusold <virusold0607@gmail.com>
2023-01-11 01:20:02 +0000

    Update files-pagination.blade.php

--- resources/views/backend/file-manager/modal/files-pagination.blade.php
+++ resources/views/backend/file-manager/modal/files-pagination.blade.php
@@ -2,14 +2,14 @@
     @foreach ($files as $file)
     <div class="col-md-3">
         <div class="card p-4 file-manager-item" id="item{{ $file->id }}" data-id="{{ $file->id }}"
-            data-file-path="{{ $file->getFileManagerThumbnailPath(400) }}">
+            data-file-path="{{ $file->getImageOptimizedFullName(400) }}">
             <div class="check-option d-none">✔</div>
             {{-- <span class="file-created-at">{{ date('F d, Y, h:i:s A', strtotime($file->created_at)) }}</span> --}}
             <span class="file-created-at">{{ $file->created_at->format('M d, Y, h:i:s A') }}</span>
             @if ($file->type != 'image')
               <img src="{{ asset('assets/img/file.svg') }}" alt="">
             @else
-              <img src="{{ $file->getFileManagerThumbnailPath(100) }}" class="card-img-top img-thumbnail" alt="{{ $file->file_name }}">
+              <img src="{{ $file->getImageOptimizedFullName(100) }}" class="card-img-top img-thumbnail" alt="{{ $file->file_name }}">
             @endif
             <div class="card-body">
                 <h5 class="card-title text-center">{{ $file->getOriginalFileFullName() }}</h5>
