db8d717f827afdb713f60a002a7909cce8c6474a
virusold <virusold0607@gmail.com>
2023-01-10 19:56:22 -0500

    fixed: getImageOptimizedFullName

--- app/Models/Upload.php
+++ app/Models/Upload.php
@@ -42,22 +42,23 @@ class Upload extends Model
 
     public function getImageOptimizedFullName($width = 0, $height = 0)
     {
-        $filename = str_replace("." . $this->extension, "", $this->file_name) . "-" . $width . "-" . $height . "." . $this->extension;
-
-        // return asset($this->fileUploadPath . "/" . $this->file_name);
-        if (file_exists(public_path($this->fileUploadPath . "/" . $filename))) {
-            return asset($this->fileUploadPath . "/" . $filename);
+        if ($height === 0) {
+            return asset($this->fileUploadPath) . '/' . $this->file_name;
         }
+
+        $filename = str_replace("." . $this->extension, "", $this->file_name) . "-" . $width . "-" . $height . "." . $this->extension;
 
         if (file_exists(public_path($this->fileUploadPath) . $this->file_name)) {
             $image = Image::make(public_path($this->fileUploadPath) . "/" . $this->file_name);
 
+            $height = $width * $image->height() / $image->width();
+
             $image->resize($width, $height);
+
             $image->save(public_path($this->fileUploadPath) . $filename, 80);
             clearstatcache();
         }
         return asset($this->fileUploadPath) . '/' . $filename;
-
     }
 
     // public function getImageCacheFullName($width = 0) {
