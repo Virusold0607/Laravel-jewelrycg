a28ecef28fb7e93b5fbf131e054447e3302f290a
virusold <virusold0607@gmail.com>
2023-01-16 12:58:11 +0000

    Update index.blade.php

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -20,9 +20,9 @@
                         <div class="blog-post-list-container">
                             <a href="{{ route('courses.show', $course->slug) }}" class="text-reset d-block">
                                 @if($course->uploads->file_name == 'none.png')
-                                    <img src="{{ asset('assets/img/placeholder.jpg') }}" alt="{{ $course->name }}" class="img-blog-cropped border lazyloaded rounded">
+                                    <img src="{{ asset('assets/img/placeholder.jpg') }}" alt="{{ $course->name }}" class="border lazyloaded rounded">
                                 @else
-                                    <img src="{{$course->uploads->getImageOptimizedFullName(600,1000)}}" alt="{{ $course->name }}" class="img-blog-cropped border lazyloaded rounded">
+                                    <img src="{{$course->uploads->getImageOptimizedFullName(600,1000)}}" alt="{{ $course->name }}" class="border lazyloaded rounded">
                                 @endif
                             </a>
                             <div class="p-2 pt-3">
