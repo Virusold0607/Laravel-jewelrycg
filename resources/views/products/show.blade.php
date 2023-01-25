6b87875966d84c8ece4d408feb703cb544cc2da5
virusold <virusold0607@gmail.com>
2023-01-25 15:19:46 +0000

    Update show.blade.php

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,19 +1,8 @@
-<x-app-layout page-title="{{$product->meta_title?$product->meta_title:$product->name}}" page-description="{{$product->meta_description}}">
 <meta property="og:image" content="{{ $product->uploads->getImageOptimizedFullName(400) }}">
 <meta property="og:image:type" content="image/png">
 <meta property="og:image:width" content="400">
 <meta property="og:image:height" content="400">
-    <style>
-        .btn-check:active + .btn, .btn-check:checked + .btn, .btn.active, .btn.show, .btn:active {
-            border: solid 1px white !important;
-        }
-
-        .min-w-50 {
-            min-width: 50px;
-        }
-
-    </style>
-
+<x-app-layout page-title="{{$product->meta_title?$product->meta_title:$product->name}}" page-description="{{$product->meta_description}}">
     <section class="product_detail_single">
         <div class="container">
             <div class="product-container col-lg-8 col-md-10 py-9 mx-auto checkout-wrap">
